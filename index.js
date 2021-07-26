7292b5b2a3149b68cb32873e673e3a26b6d854f8
allehS <69383963+Hiyoriii@users.noreply.github.com>
2021-07-26 17:29:08 +0700

    lmao

--- index.js
+++ index.js
@@ -13,9 +13,26 @@ client.on('message', async message => {
     if(!message.content.startsWith(config.prefix) || message.author.bot) return;
     const args = message.content.slice(config.prefix.length)
     if(args[1]==!undefined) return
-    if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
+    if(args=="help"){
+        let embed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`There are 5 activities available:`)
+        .setDescription('```youtube,chess,poker,betrayal,fishing```')
+        return message.channel.send({embed});}
+    if(args=="uptime"){
+        let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
+        const uptime = new Discord.MessageEmbed()
+        .setColor('RED')
+        .addField('UPTIME: ', `${days}days ${hours}hrs ${minutes}min ${seconds}sec`, true)	    
+        .setTimestamp()
+        return message.channel.send(uptime);
+    }
     switch(args){
         case"youtube":
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
         client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
             let embed = new Discord.MessageEmbed()
             .setColor('RED')
@@ -28,6 +45,7 @@ client.on('message', async message => {
         });
         break
         case"poker":
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
         client.discordTogether.createTogetherCode(message.member.voice.channelID, 'poker').then(async invite => {
             let embed = new Discord.MessageEmbed()
             .setColor('RED')
@@ -40,6 +58,7 @@ client.on('message', async message => {
         });
         break
         case"chess":
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
         client.discordTogether.createTogetherCode(message.member.voice.channelID, 'chess').then(async invite => {
             let embed = new Discord.MessageEmbed()
             .setColor('RED')
@@ -52,6 +71,7 @@ client.on('message', async message => {
         });
         break
         case"betrayal":
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
         client.discordTogether.createTogetherCode(message.member.voice.channelID, 'betrayal').then(async invite => {
             let embed = new Discord.MessageEmbed()
             .setColor('RED')
@@ -64,6 +84,7 @@ client.on('message', async message => {
         });
         break
         case"fishing":
+        if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel')
         client.discordTogether.createTogetherCode(message.member.voice.channelID, 'fishing').then(async invite => {
             let embed = new Discord.MessageEmbed()
             .setColor('RED')
@@ -75,12 +96,6 @@ client.on('message', async message => {
             return message.channel.send({embed});
         });
         break
-        case"help":
-        let embed = new Discord.MessageEmbed()
-        .setColor('RED')
-        .setTitle(`There are 5 activities available:`)
-        .setDescription('```youtube,chess,poker,betrayal,fishing```')
-        return message.channel.send({embed});
     }
 });
 
